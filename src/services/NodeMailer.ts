2f8192cfd77a9dc3bbf27c6b74b04220bc0644b3
Gabriel Henrique <p-gabriel.henrique@bbchain.com.br>
2025-03-06 17:16:57 -0300

    feat: update email template for successful purchase and modify QR code link handling

--- src/services/NodeMailer.ts
+++ src/services/NodeMailer.ts
@@ -13,28 +13,22 @@ const __dirname = dirname(__filename);
 export const mailService = {
   async sendEmail(clientEmail: string, relationShipId: string): Promise<{ message: string; messageId: string; error: boolean }> {
     try {
-      // 🔹 Link do site que será convertido em QR Code
       const siteUrl = `${process.env.FRONT_URL}/love/${relationShipId}`;
-
-      // 🔹 Gerar o QR Code em formato Base64
       const qrCodeBase64 = await QRCode.toDataURL(siteUrl);
-
-      // 🔹 Ler o template do e-mail
       const htmlFilePath = path.join(__dirname, '../mails/successfull-purchase.html');
       let htmlTemplate = fs.readFileSync(htmlFilePath, 'utf8');
 
       // 🔹 Substituir placeholders no HTML
       htmlTemplate = htmlTemplate
-        .replace('{{RELATIONSHIP_ID}}', relationShipId)
         .replace('{{CLIENT_NAME}}', 'Cliente')
-        .replace('{{QR_CODE}}', `<img src="${qrCodeBase64}" alt="QR Code" width="150"/>`)
-        .replace('{{QR_LINK}}', `<a href="${siteUrl}" class="button">Baixe o QR Code, clicando aqui!</a>`);
+        .replace('{{QR_CODE}}', `${qrCodeBase64}`)
+        .replace('{{QR_LINK}}', `${siteUrl}`);
 
       // 🔹 Enviar e-mail
       const mail = await transporter.sendMail({
         from: process.env.EMAIL_ACCOUNT,
         to: clientEmail,
-        subject: `Nova imagem adicionada ao relacionamento ${relationShipId}`,
+        subject: `Compra Finalizada!`,
         html: htmlTemplate
       });
 
